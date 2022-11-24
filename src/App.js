b19d530aaf09065130b8dd6bfb2cedfa31dede3a
Mukut Chakrabarty <mukutckb1@gmail.com>
2022-11-24 19:42:04 +0600

    header-added

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  <button className="btn btn-success">Success</button>
+     
     </div>
   );
 }
