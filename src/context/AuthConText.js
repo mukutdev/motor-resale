def53334ca0ae94c02fefe83f439ebd68ecd8e13
Mukut Chakrabarty <mukutckb1@gmail.com>
2022-11-25 16:17:14 +0600

    email-pass-auth-added

--- src/context/AuthConText.js
+++ src/context/AuthConText.js
@@ -1,13 +1,60 @@
-import React, { createContext } from 'react';
-
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut } from 'firebase/auth'
+import app from '../firebase/firebase.config';
 
 export const AuthProvider = createContext() 
 
+
+const auth = getAuth(app)
+
 const AuthConText = ({children}) => {
 
+    const [user , setUser] = useState(null)
+    const [loading , setLoading] = useState(true)
+
+    const googleProvider = new GoogleAuthProvider()
 
 
-    const authInfo = {}
+    //email password based authentication
+
+    const handleCreateUser = (email , password) => {
+        loading(true)
+        return createUserWithEmailAndPassword(auth , email, password)
+    }
+
+    //google login based authentication
+
+    const handleGoogleLogin = ()=>{
+        return signInWithPopup(auth , googleProvider)
+    }
+
+    //useLogout function
+
+    const handleUserLogout = () => {
+        return signOut(auth)
+    }
+
+
+    //watching user movement function
+
+    useEffect(()=>{
+
+        const unsubscribe = onAuthStateChanged(auth ,currentUser =>{
+            setUser(currentUser)
+            setLoading(false)
+        })
+
+        return ()=> unsubscribe()
+
+    } ,[])
+
+    
+    //handleUpdateProfile
+        
+
+
+
+    const authInfo = {user , handleCreateUser , loading , handleUserLogout , handleGoogleLogin }
 
     return (
         <AuthProvider.Provider value={authInfo}>
