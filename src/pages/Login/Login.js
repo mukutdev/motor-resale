78331b0bcef9498e0b007704cc30eacb619cec71
Mukut Chakrabarty <mukutckb1@gmail.com>
2022-11-25 13:30:16 +0600

    login-page-added

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,10 +1,61 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import { FcGoogle } from 'react-icons/fc';
 
 const Login = () => {
+    const {register , handleSubmit , formState: { errors }} = useForm()
+
+    const handleUserSubmit = data =>{
+        console.log(data);
+    }
     return (
+        <section className="bg-slate-600 h-screen">
+      <div className="absolute h-full flex flex-col justify-center items-center left-0 right-0 top-0 bottom-0">
         <div>
-            This Is Login
+          <img
+            src="https://i.ibb.co/Vpp9J31/logo-auto-parts.png"
+            className="h-9"
+            alt=""
+          />
         </div>
+        <div className="bg-base-100 md:w-[400px] mt-10 p-6 md:mx-0 mx-4">
+          <h2 className="text-center text-2xl font-bold">Login</h2>
+          <img
+            src="https://i.ibb.co/BNfMBPX/ts-Ulw-sw-RF-Giqr-Ms-U5-RCQ.png"
+            className="mt-3"
+            alt=""
+          />
+          <p className="text-center text-xl font-medium mt-2">
+            Enter your login details here
+          </p>
+          <form onSubmit={handleSubmit(handleUserSubmit)}>
+           
+            <input
+              type="text"
+              placeholder="Enter Your Email"
+              {...register("email" , { required: true })}
+              className="border-0 w-full outline-none bg-gray-300 px-3 py-3 mt-3 text-slate-900 placeholder-gray-500"
+            />
+               {errors.email && <span className="text-red-500">Please enter a valid email address</span>}
+            <input
+              type="password"
+              placeholder="Enter Your Password"
+              {...register("password" , { required: true })}
+              className="border-0 w-full outline-none bg-gray-300 px-3 py-3 mt-3 text-slate-900 placeholder-gray-500"
+            />
+            {errors.password && <span className="text-red-500">Please enter correct password</span>}     
+             <input type="submit" value="Login"  className="w-full bg-yellow-400 hover:bg-yellow-500 cursor-pointer py-3 mt-4 font-semibold text-xl"/>
+          </form>
+          <div className="mt-4">
+             <p className="font-medium text-center">New User ? <Link to={'/register'} className="underline text-lg font-semibold"> Create a account!</Link></p>
+             <div className="divider">OR</div>
+             
+             <button className='flex items-center justify-center bg-slate-600 hover:bg-slate-500 w-full py-3 text-white text-xl'><FcGoogle className='text-xl mr-2'></FcGoogle> Login using Google</button>
+          </div>
+        </div>
+      </div>
+    </section>
     );
 };
 
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 const Register = () => {
+
+    const {register , handleSubmit , formState: { errors }} = useForm()
+
+    const handleUserSubmit = data =>{
+        console.log(data);
+    }
+
   return (
     <section className="bg-slate-600 h-screen">
       <div className="absolute h-full flex flex-col justify-center items-center left-0 right-0 top-0 bottom-0">
@@ -12,7 +20,7 @@ const Register = () => {
             alt=""
           />
         </div>
-        <div className="bg-base-100 w-[400px] mt-10 p-6">
+        <div className="bg-base-100 md:w-[400px] mt-10 p-6 md:mx-0 mx-4">
           <h2 className="text-center text-2xl font-bold">Sign Up</h2>
           <img
             src="https://i.ibb.co/BNfMBPX/ts-Ulw-sw-RF-Giqr-Ms-U5-RCQ.png"
@@ -22,22 +30,28 @@ const Register = () => {
           <p className="text-center text-xl font-medium mt-2">
             Enter your Details Here
           </p>
-          <form>
+          <form onSubmit={handleSubmit(handleUserSubmit)}>
             <input
               type="text"
               placeholder="Enter Full Name"
+              {...register("name" , { required: true })}
               className="border-0 w-full outline-none bg-gray-300 px-3 py-3 mt-5 text-slate-900 placeholder-gray-500"
             />
+            {errors.name && <span className="text-red-500">Please enter your name</span>}
             <input
               type="text"
               placeholder="Enter Your Email"
+              {...register("email" , { required: true })}
               className="border-0 w-full outline-none bg-gray-300 px-3 py-3 mt-3 text-slate-900 placeholder-gray-500"
             />
+               {errors.email && <span className="text-red-500">Please enter a valid email address</span>}
             <input
               type="password"
               placeholder="Enter Your Password"
+              {...register("password" , { required: true })}
               className="border-0 w-full outline-none bg-gray-300 px-3 py-3 mt-3 text-slate-900 placeholder-gray-500"
             />
+            {errors.password && <span className="text-red-500">Please enter a strong password</span>}
             <div className="mt-3">
                 <p className="text-lg font-medium">Which type of Account You want to create?</p>
               <label className="label cursor-pointer">
@@ -45,6 +59,8 @@ const Register = () => {
                 <input
                   type="radio"
                   name="radio-10"
+                  {...register("accountMode")}
+                  value="seller"
                   className="radio checked:bg-yellow-500 "
                   
                 />
@@ -54,6 +70,8 @@ const Register = () => {
                 <input
                   type="radio"
                   name="radio-10"
+                  {...register("accountMode")}
+                  value="buyer"
                   className="radio checked:bg-yellow-500"
                   checked
                 />
